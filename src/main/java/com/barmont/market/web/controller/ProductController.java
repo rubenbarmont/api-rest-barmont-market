50171ab6dc73050322a7bb9bc65bb512a1e824c4
rubenbarmont <rubenbarmont@gmail.com>
2024-05-21 00:07:49 -0500

    API probada es postman y lista para exponer, con errores corregidos

--- src/main/java/com/barmont/market/web/controller/ProductController.java
+++ src/main/java/com/barmont/market/web/controller/ProductController.java
@@ -3,7 +3,11 @@ package com.barmont.market.web.controller;
 import com.barmont.market.domain.Product;
 import com.barmont.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,19 +26,23 @@ public class ProductController {
         return productService.getAll();
     }
 
-    public Optional<Product> getProduct(int productId) {
+    @GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") int productId) {
         return productService.getProduct(productId);
     }
 
-    public Optional<List<Product>> getByCategory(int categoryId) {
+    @GetMapping("/category/{categoryId}")
+    public Optional<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId) {
         return productService.getByCategory(categoryId);
     }
 
-    public Product save(Product product) {
+    @PostMapping("/save")
+    public Product save(@RequestBody Product product) {
         return productService.save(product);
     }
 
-    public boolean delete(int productId) {
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable("id") int productId) {
         return  productService.delete(productId);
     }
 
